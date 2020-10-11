113f78122cd99a119303f7a005b238d68bc87098
Charles Coulton <ccoulton@nevada.unr.edu>
2020-10-11 16:12:42 -0500

    Updating project with bcd, and hello-world and and 816 op codes.

--- 6502-monitor/6502-monitor.ino
+++ 6502-monitor/6502-monitor.ino
@@ -8,31 +8,36 @@ Adafruit_MCP23008 DATA;
 bool clockFlag = false;
 
 const char opcodeMatrix[256][5] = {\
-             "BRK", "ORA", ""   , "", "TSB", "ORA", "ASL", "RMB0", "PHP", "ORA", "ASL", ""   , "TSB", "ORA", "ASL", "BBR0",\
-             "BPL", "ORA", "ORA", "", "TRB", "ORA", "ASL", "RMB1", "CLC", "ORA", "INC", ""   , "TRB", "ORA", "ASL", "BBR1",\
-             "JSR", "AND", ""   , "", "BIT", "AND", "ROL", "RMB2", "PLP", "AND", "ROL", ""   , "BIT", "AND", "ROL", "BBR2",\
-             "BMI", "AND", "AND", "", "BIT", "AND", "ROL", "RMB3", "SEC", "AND", "DEC", ""   , "BIT", "AND", "ROL", "BBR3",\
-             "RTI", "EOR", ""   , "", ""   , "EOR", "LSR", "RMB4", "PHA", "EOR", "LSR", ""   , "JMP", "EOR", "LSR", "BBR4",\
-             "BVC", "EOR", "EOR", "", ""   , "EOR", "LSR", "RMB5", "CLI", "EOR", "PHY", ""   , ""   , "EOR", "LSR", "BBR5",\
-             "RTS", "ADC", ""   , "", "STZ", "ADC", "ROR", "RMB6", "PLA", "ADC", "ROR", ""   , "JMP", "ADC", "ROR", "BBR6",\
-             "BVS", "ADC", "ADC", "", "STZ", "ADC", "ROR", "RMB7", "SEI", "ADC", "PLY", ""   , "JMP", "ADC", "ROR", "BBR7",\
-             "BRA", "STA", ""   , "", "STY", "STA", "STX", "SMB0", "DEY", "BIT", "TXA", ""   , "STY", "STA", "STX", "BBS0",\
-             "BCC", "STA", "STA", "", "STY", "STA", "STX", "SMB1", "TYA", "STA", "TXS", ""   , "STZ", "STA", "STZ", "BBS1",\
-             "LDY", "LDA", "LDX", "", "LDY", "LDA", "LDX", "SMB2", "TAY", "LDA", "TAX", ""   , "LDY", "LDA", "LDX", "BBS2",\
-             "BCS", "LDA", "LDA", "", "LDY", "LDA", "LDX", "SMB3", "CLV", "LDA", "TSX", ""   , "LDY", "LDA", "LDX", "BBS3",\
-             "CPY", "CMP", ""   , "", "CPY", "CMP", "DEC", "SMB4", "INY", "CMP", "DEX", "WAI", "CPY", "CMP", "DEC", "BBS4",\
-             "BNE", "CMP", "CMP", "", ""   , "CMP", "DEC", "SMB5", "CLD", "CMP", "PHX", "STP", ""   , "CMP", "DEC", "BBS5",\
-             "CPX", "SBC", ""   , "", "CPX", "SBC", "INC", "SMB6", "INX", "SBC", "NOP", ""   , "CPX", "SBC", "INC", "BBS6",\
-             "BEQ", "SBC", "SBC", "", ""   , "SBC", "INC", "SMB7", "SED", "SBC", "PLX", ""   , ""   , "SBC", "INC", "BBS7"};
+        "BRK", "ORA", "COP", "ORA", "TSB", "ORA", "ASL", "RMB0", "PHP", "ORA", "ASL", "PHD", "TSB", "ORA", "ASL", "BBR0",\
+        "BPL", "ORA", "ORA", "ORA", "TRB", "ORA", "ASL", "RMB1", "CLC", "ORA", "INC", "TCS", "TRB", "ORA", "ASL", "BBR1",\
+        "JSR", "AND", "JSL", "AND", "BIT", "AND", "ROL", "RMB2", "PLP", "AND", "ROL", "PLD", "BIT", "AND", "ROL", "BBR2",\
+        "BMI", "AND", "AND", "AND", "BIT", "AND", "ROL", "RMB3", "SEC", "AND", "DEC", "TSC", "BIT", "AND", "ROL", "BBR3",\
+        "RTI", "EOR", "WDM", "EOR", "MVP", "EOR", "LSR", "RMB4", "PHA", "EOR", "LSR", "PHK", "JMP", "EOR", "LSR", "BBR4",\
+        "BVC", "EOR", "EOR", "EOR", "MVN", "EOR", "LSR", "RMB5", "CLI", "EOR", "PHY", "TCD", "JMP", "EOR", "LSR", "BBR5",\
+        "RTS", "ADC", "PER", "ADC", "STZ", "ADC", "ROR", "RMB6", "PLA", "ADC", "ROR", "RTL", "JMP", "ADC", "ROR", "BBR6",\
+        "BVS", "ADC", "ADC", "ADC", "STZ", "ADC", "ROR", "RMB7", "SEI", "ADC", "PLY", "TDC", "JMP", "ADC", "ROR", "BBR7",\
+        "BRA", "STA", "BRL", "STA", "STY", "STA", "STX", "SMB0", "DEY", "BIT", "TXA", "PHB", "STY", "STA", "STX", "BBS0",\
+        "BCC", "STA", "STA", "STA", "STY", "STA", "STX", "SMB1", "TYA", "STA", "TXS", "TXY", "STZ", "STA", "STZ", "BBS1",\
+        "LDY", "LDA", "LDX", "LDA", "LDY", "LDA", "LDX", "SMB2", "TAY", "LDA", "TAX", "PLB", "LDY", "LDA", "LDX", "BBS2",\
+        "BCS", "LDA", "LDA", "LDA", "LDY", "LDA", "LDX", "SMB3", "CLV", "LDA", "TSX", "TYX", "LDY", "LDA", "LDX", "BBS3",\
+        "CPY", "CMP", "REP", "CMP", "CPY", "CMP", "DEC", "SMB4", "INY", "CMP", "DEX", "WAI", "CPY", "CMP", "DEC", "BBS4",\
+        "BNE", "CMP", "CMP", "CMP", "PEI", "CMP", "DEC", "SMB5", "CLD", "CMP", "PHX", "STP", "JML", "CMP", "DEC", "BBS5",\
+        "CPX", "SBC", "SEP", "SBC", "CPX", "SBC", "INC", "SMB6", "INX", "SBC", "NOP", "XBA", "CPX", "SBC", "INC", "BBS6",\
+        "BEQ", "SBC", "SBC", "SBC", "PEA", "SBC", "INC", "SMB7", "SED", "SBC", "PLX", "XCE", "JSR", "SBC", "INC", "BBS7"};
 
 #define CLOCK 7
 #define READ_WRITE 5
 #define LED 13
-#define SYNC 9
-SoftwareSerial aciaSerial(MISO, MOSI);
+#define VDA 9
+#define RDY 10
+#define IRQ 11
+#define VPA 12
+SoftwareSerial aciaSerial(MOSI, MISO);
+char buffer[20];
+int bufidx = 0;
 void setup() {
-  ADDR.begin(0);
-  DATA.begin(1);
+  ADDR.begin((uint8_t)0);
+  DATA.begin((uint8_t)1);
   Serial.begin(9600);
   aciaSerial.begin(19200);
   for (int n = 0; n < 16; n++) {
@@ -44,7 +49,11 @@ void setup() {
   pinMode(CLOCK, INPUT);
   pinMode(READ_WRITE, INPUT);
   pinMode(LED, OUTPUT);
-  pinMode(SYNC, INPUT);
+  pinMode(VPA, INPUT);
+  pinMode(VDA, INPUT);
+  pinMode(RDY, OUTPUT);
+  //digitalWrite(RDY, HIGH);
+  pinMode(IRQ, INPUT);
   attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
 }
 
@@ -55,8 +64,16 @@ void onClock() {
 void loop(){
   if (aciaSerial.available()){
     char incoming = aciaSerial.read();
-    Serial.print(incoming);
-    aciaSerial.write("\n");
+    buffer[bufidx++] = incoming;
+    if (incoming == 0x0a) {
+      //digitalWrite(RDY, LOW);
+      Serial.println(buffer);
+      while(!Serial.available()){
+        delay(1000);
+      }
+      aciaSerial.write(Serial.read());
+      //digitalWrite(RDY, HIGH);
+    }
   }
   if (clockFlag) {
     detachInterrupt(digitalPinToInterrupt(CLOCK));
@@ -67,8 +84,9 @@ void loop(){
 
     uint8_t hexdata = DATA.readGPIO();
 
-    sprintf(output, "%04x %c %02x instr:%4s", hexaddress, digitalRead(READ_WRITE) ? 'r' : 'W', hexdata,
-      digitalRead(SYNC)?opcodeMatrix[hexdata]:"EXE");
+    sprintf(output, "%04x %c %02x instr:%4s", hexaddress, 
+      digitalRead(READ_WRITE) ? 'r' : 'W', hexdata,
+      digitalRead(VDA&&VPA) ? opcodeMatrix[hexdata]:"EXE");
     Serial.println(output);
     attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
   }
--- 6502Code/src/hello-world.asm
+++ 6502Code/src/hello-world.asm
@@ -107,21 +107,18 @@ print_char:
     and #LCD_RS
     sta PORTA
     rts
-;res = 00ffe0
-;res = 00ffe2
+
 .org $ffe4
-.word 0000 ;cop_isr_nat = 00ffe4
-.word 0000 ;brk_isr_nat = 00ffe6
-.word 0000 ;abt_isr_nat = 00ffe8
-.word 0000 ;nmi_isr_nat = 00ffea
-.word $eaea;res         = 00ffec
-.word 0000 ;irq_isr_nat = 00ffee 
-;.org $fff0 ;irq vectors
-.word $eaea ;reserved    = 00fff0
-.word $eaea ;reserved    = 00fff2
-.word 0000 ;cop_ISR_emu = 00fff4
+.word $0000 ;cop_isr_nat = 00ffe4
+.word $0000 ;brk_isr_nat = 00ffe6
+.word $0000 ;abt_isr_nat = 00ffe8
+.word $0000 ;nmi_isr_nat = 00ffea
+.word $eaea ;res         = 00ffec
+.word $0000 ;irq_isr_nat = 00ffee 
+.org  $fff4 ;irq vectors
+.word $8000 ;cop_ISR_emu = 00fff4
 .word $eaea ;reserved    = 00fff6
-.word 0000 ;abt_ISR_emu = 00fff8
-.word 0000 ;nmi_ISR_emu = 00fffa
-.word reset;rst 00fffc 
-.word 0000 ;cnt_isr 00fffe
+.word $8000 ;abt_ISR_emu = 00fff8
+.word $8000 ;nmi_ISR_emu = 00fffa
+.word reset ;rst         = 00fffc 
+.word $8000 ;cnt_isr     = 00fffe
