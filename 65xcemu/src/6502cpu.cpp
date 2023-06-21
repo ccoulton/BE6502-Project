4f134fca5f919e1a30e7b99685607e62e47b997d
Charles Coulton <ccoulton@vermeer.com>
2023-06-21 16:44:55 -0500

    Removed artifacts and updating branch code.

--- 65xcemu/src/6502cpu.cpp
+++ 65xcemu/src/6502cpu.cpp
@@ -30,25 +30,45 @@ void CPU::fetchWordintoRegister(uint16 address, Register* output, int* cycle) {
     output->bytes.h = fetchValueFromAddress(address, cycle);
 }
 
+void checkpageboundry(uint16 prev, uint16 curr, int* cycle) {
+    (*cycle)--;
+}
+
 void CPU::run(int clock) {
-  Reset(&clock);
-  while (clock > 0) {
-      intReg.reg = fetchValueFromAddress(PC.r++, &clock);
-      uint8 lownibble = intReg.reg & 0x0f;
-      if (lownibble == 0x08) {
+    auto getcondflag = [this](const int value) {
+        return  (value == negative) ? PS.flags.n  :
+                (value == overflow) ? PS.flags.v  :
+                (value == carry)    ? PS.flags.ce :
+                                      PS.flags.z;
+    };
+    Reset(&clock);
+    while (clock > 0) {
+        intReg.reg = fetchValueFromAddress(PC.r++, &clock);
+        if (intReg.cond.id == 0x10) {
+            if (getcondflag(intReg.cond.x) != intReg.cond.y) {
+                PC.r += 2;
+                clock--;
+            } else {
+                fetchWordintoRegister(PC.r, &PC, &clock);
+                //if page boundry clock--;
+            }
+            continue;
+        } 
+        uint8 lownibble = intReg.reg & 0x0f;
+        if (lownibble == 0x08) {
         //php/clc/plp/sec/pha/cli/pla/sei/dey/tya/tay/clv/iny/cld/inx/sed
-      } else if (lownibble == 0x0B) {
+        } else if (lownibble == 0x0B) {
         //phd/tcs/pld/tsc/phk/tcd/rtl/tdc/phb/txy/plb/tyx/wai/stp/xba/xce
-      } else if (lownibble == 0x0A) {
-          if (intReg.reg >= 0x7A) {
-              //ply, txa, txs, tax, tsx dex, phx, plx
-              if (intReg.reg == 0xEA) { //inc:111 acc:010, group2:10
-                  clock--; //nop
-              }
-              continue;
-          } else if (intReg.reg == 0x5A) { //phy
-          }
-      } //0x42 wdm, 0x40 rti, 0x60 rts
-  }
-  return;
+        } else if (lownibble == 0x0A) {
+            if (intReg.reg >= 0x7A) {
+                //ply, txa, txs, tax, tsx dex, phx, plx
+                if (intReg.reg == 0xEA) { //inc:111 acc:010, group2:10
+                    clock--; //nop
+                }
+                continue;
+            } else if (intReg.reg == 0x5A) { //phy
+            }
+        } //0x42 wdm, 0x40 rti, 0x60 rts
+    }
+    return;
 }
